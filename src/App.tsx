d6b1ceeaf98bfe9a27c99e193e9380d7d28512bc
Pedro Soares <pedropaulos.1998@gmail.com>
2022-12-29 19:29:52 -0300

    implemented routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { Router } from "./routes"
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/theme/default";
 
 function App() {
 
   return (
-    <div> </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
---
+++ src/pages/CompleteOrder/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CompleteOrder = () => {
+  return (
+    <h1>CompleteOrder</h1>
+  )
+}
